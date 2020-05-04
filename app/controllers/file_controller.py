de9cc0d3ea409b28de089283ac57ca7a65458223
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2020-05-04 15:00:47 +0700

    example validate file upload

--- app/controllers/file_controller.py
+++ app/controllers/file_controller.py
@@ -7,7 +7,7 @@
 """
 
 from app import storage
-from app.utils.response import respond_json
+from app.utils.response import respond_json, respond_json_with_validation_errors
 from flask import request
 from flask.blueprints import Blueprint
 
@@ -16,6 +16,11 @@ mod = Blueprint("file_controller", __name__, url_prefix='/api')
 
 @mod.route('/files', methods=['POST'])
 def store():
+    if 'file' not in request.files or request.files['file'].filename == '':
+        return respond_json_with_validation_errors({
+            'file': ['file doest not exists!']
+        })
+
     file = storage.put('my_files', request.files['file'])
 
     return respond_json(
