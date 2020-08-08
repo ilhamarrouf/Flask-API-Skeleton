b2e24a6aae2ef28f7a466fb17a47f8588831e17c
ilhamarrouf <ilham.arrouf@gmail.com>
2020-08-08 22:09:07 +0700

    remove old validation

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -6,14 +6,12 @@
     :time: 09.59
 """
 
-from app import db
 from app.models.user import User
-from app.requests.user import UserRequest
 from app.utils import paginator
-from app.utils.response import respond_json, respond_json_with_validation_errors
-from flask import abort, Blueprint, request, jsonify, url_for
+from app.utils.response import respond_json
+from flask import Blueprint, request
 from sqlalchemy.sql.expression import desc
-from werkzeug.exceptions import BadRequest
+
 
 mod = Blueprint("user_controller", __name__, url_prefix="/api")
 
@@ -32,35 +30,4 @@ def index():
         meta={
             'pagination': paginator.paginate(users)
         }
-    )
-
-
-@mod.route("/users", methods=["POST"])
-def store():
-    form = UserRequest(request.form)
-
-    if not form.validate():
-        return respond_json_with_validation_errors(form.errors)
-
-    user = User(username=form.username.data)
-    user.hash_password(form.password.data)
-
-    try:
-        db.session.add(user)
-        db.session.commit()
-    except Exception as exception:
-        db.session.rollback()
-
-        return respond_json(
-            success=False,
-            message=getattr(exception, 'message', repr(exception)),
-            data=None,
-            code=BadRequest.code
-        )
-
-    return respond_json(
-        success=True,
-        message="successfully added user",
-        data=user.serialize,
-        code=200
     )
